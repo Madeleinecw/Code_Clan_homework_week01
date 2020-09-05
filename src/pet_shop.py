8186b3c6ae477ebc6628a5da373dba5d51547fa4
Madeleine Wood <madeleinecwood@gmail.com>
2020-09-05 12:33:46 +0100

    added get_customer_cash

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -49,4 +49,6 @@ def remove_pet_by_name(shop, name):
         
 def add_pet_to_stock(shop,new_pet):
     shop["pets"].append(new_pet)
-           
+
+def get_customer_cash(customer):
+    return customer["cash"]
