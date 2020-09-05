5d1cde060f1695b5962c73d035d50c80edf175af
Madeleine Wood <madeleinecwood@gmail.com>
2020-09-05 11:22:56 +0100

    added add_pet_to_stock

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,24 +1,24 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(list):
-    return list["name"]
+def get_pet_shop_name(shop):
+    return shop["name"]
 
-def get_total_cash(list):
-    return list["admin"]["total_cash"]
+def get_total_cash(shop):
+    return shop["admin"]["total_cash"]
 
-def add_or_remove_cash(list, num_1):
-    list["admin"]["total_cash"] += num_1
-    return list["admin"]["total_cash"]
+def add_or_remove_cash(shop, num_1):
+    shop["admin"]["total_cash"] += num_1
+    return shop["admin"]["total_cash"]
 
-def get_pets_sold(list):
-    return list["admin"]["pets_sold"]
+def get_pets_sold(shop):
+    return shop["admin"]["pets_sold"]
 
-def increase_pets_sold(list, num_1):
-    list["admin"]["pets_sold"] += num_1
-    return list["admin"]["pets_sold"]
+def increase_pets_sold(shop, num_1):
+    shop["admin"]["pets_sold"] += num_1
+    return shop["admin"]["pets_sold"]
 
-def get_stock_count(list):
-    return len(list["pets"])
+def get_stock_count(shop):
+    return len(shop["pets"])
 
 def get_pets_by_breed(shop, breed):
     pets_list = []
@@ -47,4 +47,6 @@ def remove_pet_by_name(shop, name):
             del(shop["pets"][index])
             return
         
-        
+def add_pet_to_stock(shop,new_pet):
+    shop["pets"].append(new_pet)
+           
